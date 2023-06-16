1c015b67fb4a6e979b175b15b0d514a9e4ea55de
MaViProg <viktrija-iknnikva@yandex.ru>
2023-06-16 12:44:44 +0300

    fix comments

--- src/main/java/com/ikonnikova/restrailway/conroller/CargoController.java
+++ src/main/java/com/ikonnikova/restrailway/conroller/CargoController.java
@@ -13,39 +13,69 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Rest controller for managing cargos.
+ */
+
 @RestController
 @RequestMapping("/api/cargos")
-@Tag(name = "Cargo API", description = "API для управления грузами")
+@Tag(name = "Cargo API", description = "API for managing cargos")
 public class CargoController {
 
     @Autowired
     private CargoRepository cargoRepository;
 
+    /**
+     * Create a new cargo.
+     *
+     * @param cargo The cargo to create.
+     * @return The created cargo.
+     */
     @PostMapping
-    @Operation(summary = "Создание груза")
+    @Operation(summary = "Create a cargo")
     public Cargo createCargo(@RequestBody Cargo cargo) {
         return cargoRepository.save(cargo);
     }
 
+    /**
+     * Get a cargo by ID.
+     *
+     * @param id The ID of the cargo.
+     * @return The cargo with the specified ID.
+     * @throws EntityNotFoundException if the cargo is not found.
+     */
     @GetMapping("/{id}")
-    @Operation(summary = "Получение информации о грузе по ID")
+    @Operation(summary = "Get a cargo by ID")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Информация о грузе успешно получена"),
-            @ApiResponse(responseCode = "404", description = "Груз не найден")
+            @ApiResponse(responseCode = "200", description = "Cargo information received successfully"),
+            @ApiResponse(responseCode = "404", description = "Cargo not found")
     })
     public Cargo getCargoById(@PathVariable Long id) {
         return cargoRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Cargo not found"));
     }
 
+    /**
+     * Get all cargos.
+     *
+     * @return The list of all cargos.
+     */
     @GetMapping
-    @Operation(summary = "Получение списка всех грузов")
+    @Operation(summary = "Get all cargos")
     public List<Cargo> getAllCargos() {
         return cargoRepository.findAll();
     }
 
+    /**
+     * Update a cargo.
+     *
+     * @param id           The ID of the cargo to update.
+     * @param updatedCargo The updated cargo data.
+     * @return The updated cargo.
+     * @throws EntityNotFoundException if the cargo is not found.
+     */
     @PutMapping("/{id}")
-    @Operation(summary = "Обновление информации о грузе")
+    @Operation(summary = "Update a cargo")
     public Cargo updateCargo(@PathVariable Long id, @RequestBody Cargo updatedCargo) {
         Cargo cargo = cargoRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Cargo not found"));
@@ -54,8 +84,15 @@ public class CargoController {
         return cargoRepository.save(cargo);
     }
 
+    /**
+     * Delete a cargo by ID.
+     *
+     * @param id The ID of the cargo to delete.
+     * @return ResponseEntity with no content if the cargo is successfully deleted.
+     * @throws EntityNotFoundException if the cargo is not found.
+     */
     @DeleteMapping("/{id}")
-    @Operation(summary = "Удаление груза по ID")
+    @Operation(summary = "Delete a cargo by ID")
     public ResponseEntity<?> deleteCargoById(@PathVariable Long id) {
         Cargo cargo = cargoRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Cargo not found"));
