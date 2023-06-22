f030cb5e85ac083d9b9adf42ea47b6ccf73155c3
MaViProg <viktrija-iknnikva@yandex.ru>
2023-06-22 15:32:37 +0300

    add comments receiveWagons(), moveWagons()

--- src/main/java/com/ikonnikova/restrailway/service/WagonService.java
+++ src/main/java/com/ikonnikova/restrailway/service/WagonService.java
@@ -35,48 +35,72 @@ public class WagonService {
      * @param stationTrackId идентификатор пути станции
      */
     public void receiveWagons(List<Long> wagonsId, Long stationTrackId) {
+
+        // Получаем путь станции, на которую будут приняты вагоны
         StationTrack stationTrack = stationTrackRepository.findById(stationTrackId)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid station track ID"));
 
+        // Получаем последний вагон на данном пути станции, сортируя по позиции в обратном порядке
         Wagon lastWagon = wagonRepository.findTopByStationTrackOrderByPositionDesc(stationTrack);
+
+        // Инициализируем переменную позиции вагона
         int position = lastWagon != null ? lastWagon.getPosition() : 1;
 
+        // Перебираем каждый вагон из списка идентификаторов вагонов
         for (Wagon wagon : wagonRepository.findAllById(wagonsId)) {
+
+            // Устанавливаем путь станции для вагона
             wagon.setStationTrack(stationTrack);
+
+            // Устанавливаем позицию вагона в зависимости от значения переменной позиции
             wagon.setPosition(position == 0 ? WagonMovePosition.HEAD : WagonMovePosition.TAIL);
+
+            // Сохраняем вагон в репозитории
             wagonRepository.save(wagon);
+
+            // Обновляем значение переменной позиции на 0 для следующего вагона
             position = 0;
         }
     }
+
 
     /**
      * Операция перестановки вагонов внутри станции.
      * На входе список вагонов и путь на который они будут перемещены.
      * Вагоны могут быть перемещены только в начало или конец состава.
      *
-     * @param wagons          список идентификаторов вагонов
-     * @param stationTrackId  идентификатор пути станции
-     * @param direction       направление перемещения вагонов
+     * @param wagons         список идентификаторов вагонов
+     * @param stationTrackId идентификатор пути станции
+     * @param direction      направление перемещения вагонов
      */
     public void moveWagons(List<Long> wagons, Long stationTrackId, WagonMovePosition direction) {
+
+        // Получаем путь станции, на который будут перемещены вагоны
         StationTrack destinationStationTrack = stationTrackRepository.findById(stationTrackId)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid destination station track ID"));
 
+        // Инициализируем переменные для определения первого и последнего вагонов
         Wagon firstWagon = null;
         Wagon lastWagon = null;
 
+        // Перебираем каждый идентификатор вагона из списка
         for (Long wagonId : wagons) {
+            // Получаем объект вагона по его идентификатору
             Wagon wagon = wagonRepository.findById(wagonId)
                     .orElseThrow(() -> new IllegalArgumentException("Invalid wagon ID"));
 
+            // Определяем первый вагон или вагон с позицией 0
             if (firstWagon == null || wagon.getPosition() == 0) {
                 firstWagon = wagon;
             }
 
+            // Определяем последний вагон или вагон с позицией 1
             if (lastWagon == null || wagon.getPosition() == 1) {
                 lastWagon = wagon;
             }
 
+            // Если путь станции вагона не совпадает с целевым путем,
+            // изменяем путь станции на целевой путь и устанавливаем позицию вагона в начало состава
             if (!wagon.getStationTrack().equals(destinationStationTrack)) {
                 wagon.setStationTrack(destinationStationTrack);
                 wagon.setPosition(WagonMovePosition.HEAD);
@@ -84,22 +108,31 @@ public class WagonService {
             }
         }
 
+        // Проверяем, что первый и последний вагоны определены
         if (firstWagon == null || lastWagon == null) {
             throw new IllegalStateException("Wagons list is empty");
         }
 
+        // Инициализируем переменную позиции в зависимости от направления перемещения
         int position = (direction == WagonMovePosition.HEAD) ? 1 : 0;
 
+        // Перебираем каждый идентификатор вагона из списка
         for (Long wagonId : wagons) {
+            // Получаем объект вагона по его идентификатору
             Wagon wagon = wagonRepository.findById(wagonId)
                     .orElseThrow(() -> new IllegalArgumentException("Invalid wagon ID"));
 
+            // Если путь станции вагона совпадает с целевым путем,
+            // устанавливаем позицию вагона в зависимости от значения переменной позиции
             if (wagon.getStationTrack().equals(destinationStationTrack)) {
                 wagon.setPosition((position == 0) ? WagonMovePosition.HEAD : WagonMovePosition.TAIL);
                 wagonRepository.save(wagon);
 
+                // Обновляем значение переменной позиции
                 position = (position == 0) ? 1 : 0;
             }
         }
     }
+
 }
+
