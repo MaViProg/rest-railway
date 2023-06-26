0c008ab62c8ce5972eb250c2c019cd9401978cce
MaViProg <viktrija-iknnikva@yandex.ru>
2023-06-26 15:45:49 +0300

    fix departWagons()

--- src/main/java/com/ikonnikova/restrailway/service/WaybillService.java
+++ src/main/java/com/ikonnikova/restrailway/service/WaybillService.java
@@ -1,6 +1,5 @@
 package com.ikonnikova.restrailway.service;
 
-import com.ikonnikova.restrailway.entity.StationTrack;
 import com.ikonnikova.restrailway.entity.Wagon;
 import com.ikonnikova.restrailway.entity.Waybill;
 import com.ikonnikova.restrailway.repository.WagonRepository;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import java.util.Comparator;
 import java.util.List;
 
 
@@ -42,6 +40,7 @@ public class WaybillService {
 
     /**
      * Операция убытия вагонов на сеть РЖД.
+     * 1 variant
      * Вагоны могут убывать только с начала состава.
      * <p>
      * Алгоритм действий:
@@ -54,37 +53,78 @@ public class WaybillService {
      *
      * @param waybillId идентификатор накладной
      */
+//    @Transactional
+//    public void departWagons(Long waybillId) {
+//        Waybill waybill = waybillRepository.findById(waybillId)
+//                .orElseThrow(() -> new IllegalArgumentException("Invalid waybill ID"));
+//
+//        // Получаем первый вагон в накладной
+//        Wagon firstWagon = wagonRepository.findFirstWagonByWaybill(waybill);
+//        if (firstWagon == null) {
+//            throw new IllegalStateException("No wagons found in waybill");
+//        }
+//
+//        // Проверка, стоит ли первый вагон в начале поезда
+//        StationTrack stationTrack = firstWagon.getStationTrack();
+//        if (stationTrack == null) {
+//            throw new IllegalStateException("Wagon " + firstWagon.getNumber() + " does not have a station track");
+//        }
+//
+//        // Получаем все вагоны в накладной с помощью метода findAllWagonsByWaybill
+//        List<Wagon> wagons = wagonRepository.findAllWagonsByWaybill(waybill);
+//
+//        // Сортируем вагоны по положению
+//        wagons.sort(Comparator.comparing(Wagon::getPosition));
+//
+//        // Проверка, находится ли первый вагон в начале поезда, сравниваем его с первым вагоном в отсортированном списке.
+//        if (!wagons.get(0).equals(firstWagon)) {
+//            throw new IllegalStateException("Wagons can only be departed from the beginning of the train");
+//        }
+//
+//        // Удаляем все вагоны из накладной методом deleteAllByWaybill, если первый вагон стоит в начале состава
+//        wagonRepository.deleteAllByWaybill(waybill);
+//    }
+
+    /**
+     * Операция убытия вагонов на сеть РЖД.
+     * Вагоны могут убывать только с начала состава.
+     *
+     * Алгоритм действий:
+     * 1. Получаем первый вагон в накладной с помощью метода findFirstWagonByWaybill.
+     * 3. Получаем все вагоны в накладной с помощью метода findAllWagonsByWaybill.
+     * 5. Проверяем, находится ли первый вагон в начале поезда, сравнив его с первым вагоном в отсортированном списке.
+     * 6. Если первый вагон стоит в начале состава, удаляем все вагоны из накладной методом deleteAllByWaybill.
+     *
+     * @param waybillId идентификатор накладной
+     */
     @Transactional
     public void departWagons(Long waybillId) {
+        // Получение накладной по ее ID
         Waybill waybill = waybillRepository.findById(waybillId)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid waybill ID"));
 
-        // Получаем первый вагон в накладной
-        Wagon firstWagon = wagonRepository.findFirstWagonByWaybill(waybill);
-        if (firstWagon == null) {
+        // Получаем все вагоны в накладной
+        List<Wagon> wagons = wagonRepository.findAllWagonsByWaybill(waybill);
+
+        // Проверяем наличие вагонов в накладной
+        if (wagons.isEmpty()) {
             throw new IllegalStateException("No wagons found in waybill");
         }
 
-        // Проверка, стоит ли первый вагон в начале поезда
-        StationTrack stationTrack = firstWagon.getStationTrack();
-        if (stationTrack == null) {
-            throw new IllegalStateException("Wagon " + firstWagon.getNumber() + " does not have a station track");
+        // Получаем позицию первого вагона в накладной
+        int firstWagonPosition = wagons.get(0).getPosition();
+
+        // Проверяем, находятся ли все вагоны в начале поезда
+        for (int i = 1; i < wagons.size(); i++) {
+            if (wagons.get(i).getPosition() != firstWagonPosition + i) {
+                throw new IllegalStateException("Wagons can only be departed from the beginning of the train");
+            }
         }
 
-        // Получаем все вагоны в накладной с помощью метода findAllWagonsByWaybill
-        List<Wagon> wagons = wagonRepository.findAllWagonsByWaybill(waybill);
-
-        // Сортируем вагоны по положению
-        wagons.sort(Comparator.comparing(Wagon::getPosition));
-
-        // Проверка, находится ли первый вагон в начале поезда, сравниваем его с первым вагоном в отсортированном списке.
-        if (!wagons.get(0).equals(firstWagon)) {
-            throw new IllegalStateException("Wagons can only be departed from the beginning of the train");
-        }
-
-        // Удаляем все вагоны из накладной методом deleteAllByWaybill, если первый вагон стоит в начале состава
+        // Удаляем все вагоны из накладной
         wagonRepository.deleteAllByWaybill(waybill);
     }
+
 
 }
 
